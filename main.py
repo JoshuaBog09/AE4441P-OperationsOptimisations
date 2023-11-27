39d4d4744c22e3c174354542560774fe3744d9a5
Bartosz <b.s.jemiol@student.tudelft.nl>
2023-11-27 14:21:20 +0100

    Adding docs

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello world!')
+
+class Square_Obsticale:
+    def __init__(self, x_lower_left, y_lower_left, x_upper_right, y_upper_right):
+        self.x_lower_left = x_lower_left
+        self.y_lower_left = y_lower_left
+        self.x_upper_right = x_upper_right
+        self.y_upper_right = y_upper_right
+
+
+list_of_obsticales = []
+
+# Limits of the map, width is 600px, height is 400px
+
+list_of_obsticales.append(Square_Obsticale(100, 100, 150, 150))
