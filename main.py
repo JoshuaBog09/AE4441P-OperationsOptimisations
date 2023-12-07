636e3b08858a6bb56bf1d6f48ec42bd3bb78ebf1
Justin Dubois <r37ja8kf@jpgdubois.anonaddy.com>
2023-12-07 23:47:15 +0100

    more readable, pretty code

--- main.py
+++ main.py
@@ -1,162 +1,136 @@
 import gurobipy as gp
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import math
 import numpy as np
-
-
-class SquareObsticale:
-    def __init__(self, x_lower_left, y_lower_left, x_upper_right, y_upper_right):
-        self.x_lower_left = x_lower_left
-        self.y_lower_left = y_lower_left
-        self.x_upper_right = x_upper_right
-        self.y_upper_right = y_upper_right
 
 
 list_of_obstacles = []
 
 # Limits of the map, width is 600px, height is 400px
 
+# Create MILP model.
 model = gp.Model("Path Planning")
 
-# list_of_obstacles.append(SquareObsticale(250, -20, 300, 300))
-list_of_obstacles.append(SquareObsticale(330, 30, 400, 410))
-# list_of_obstacles.append(SquareObsticale(100, 0, 125, 100))
-list_of_obstacles.append(SquareObsticale(250, -10, 300, 150))
-# Creation of variables
+# Model constants
+R = 1e6         # Big number.
+NORMALS = 60    # Number of normals used for vector magnitude calcualtion.
+DIMENSION = 2   # Number of dimensions.
 
-# initial conditions
-v_init = [0, 0]
+# Define obstacles.
+list_of_obstacles.append(np.array([[150, 200], [200, 410]]))    # Obstacle bounds (dx2 array with [lower_left, upper_right])
+list_of_obstacles.append(np.array([[10, -10], [30, 250]]))
 
-# max velocities
-v_max = 10
+# Initial conditions of the vehicle.
+x_init = [0, 0]   # Initial position of the vehicle.
+v_init = [0, 0]     # Initial velocity of the vehicle.
 
-# max input
-u_max = 0.1
+# Final conditions of the vehicle at the goal.
+x_goal = (300, 250) # Goal position
 
-# big number
-
-R = 1e6
-
-# this is equivalent to the number of time steps, L
+# Equivalent number of time steps, L.
 number_of_time_steps = 220
-# how many normals to use
 
-normals = 60
+# Limit on parameters
+map_bound = np.array([[0, 0], [400, 600]])  # Map bounds (dx2 array with [lower_left, upper_right])
+v_max = 10                                  # Maximum velocity (scalar)
+u_max = 0.1                                 # Maximum input (scalar)
 
-# initial position of the thing
-x_begin = (10, 50)
+# Define variable dictionaries.
+x = {}          # Decision variables for the position of the rover.
+u = {}          # Decision variables for the inputs of the rover.
+b_goal = {}     # Boolean indicating whether a node reaches the goal.
+b_in = {}       # Boolean used for checking collision with obstacles.
 
-# final position of the robot
-x_goal = (250, 300)
+# ================ DEFINE MODEL VARIABLES =========================
 
-# map limits
-
-x_lim = [0, 600]
-y_lim = [0, 400]
-
-x = {}
-y = {}
-ux = {}
-uy = {}
-b_goal = {}
-b_in = {}
-
+# Define the variables for each time step.
 for i in range(number_of_time_steps):
-    # Continuous variables for the position of the robot
-    x[i] = model.addVar(vtype=gp.GRB.CONTINUOUS, name=f"x_{i},0", lb=x_lim[0], ub=x_lim[1])
-    y[i] = model.addVar(vtype=gp.GRB.CONTINUOUS, name=f"x_{i},1", lb=y_lim[0], ub=y_lim[1])
-    # Control input vector (math magic)
-    ux[i] = model.addVar(vtype=gp.GRB.CONTINUOUS, name=f"u_{i},0", lb=-u_max, ub=u_max)
-    uy[i] = model.addVar(vtype=gp.GRB.CONTINUOUS, name=f"u_{i}, 1", lb=-u_max, ub=u_max)
 
-    # Binary variable to indicate if the goal is reached
-    b_goal[i] = model.addVar(vtype=gp.GRB.BINARY, name=f"b_goal_{i}")
+    for d in range(DIMENSION):
+        # Position variables for the vehicle position, limited by the map bounds.
+        x[i, d] = model.addVar(vtype=gp.GRB.CONTINUOUS, name=f"X[time={i},dim={d}]", lb=map_bound[0, d], ub=map_bound[1, d])
 
-for i in range(len(list_of_obstacles)):
-    for j in range(4):
-        for k in range(number_of_time_steps):
-            b_in[i, j, k] = model.addVar(vtype=gp.GRB.BINARY, name=f"b_in_{i},{j},{k}")
+        # Input variables, limited by maximum input.
+        u[i, d] = model.addVar(vtype=gp.GRB.CONTINUOUS, name=f"U[time={i},dim={d}]", lb=-u_max, ub=u_max)
+
+        # Binary for obstacle detection.
+        for k in range(2):
+            # Binaries for detecting obstacle collision.
+            b_in[i, d, k, 0] = model.addVar(vtype=gp.GRB.BINARY, name=f"B_in[time={i},dim={d},obs={k},bound={0}]")
+            b_in[i, d, k, 1] = model.addVar(vtype=gp.GRB.BINARY, name=f"B_in[time={i},dim={d},obs={k},bound={1}]")
+
+    # Binary indicating first node that reaches goal.
+    b_goal[i] = model.addVar(vtype=gp.GRB.BINARY, name=f"B_goal_{i}")
 
 model.update()
 
-# Creation of constraints
+# =========== CREATE MODEL CONSTRAINTS ==========================
 
-# Constraint for starting position
-model.addLConstr(x[0], '=', x_begin[0], name="C0_x")
-model.addLConstr(y[0], '=', x_begin[1], name="C0_y")
+# Constraints for forcing the initial conditions.
+CONST_X_INIT = {}
+CONST_V_INIT = {}
 
-# Constraint for starting velocity
-model.addLConstr(x[1], '=', x[0] + v_init[0] + ux[0])
-model.addLConstr(y[1], '=', y[0] + v_init[1] + uy[0])
+for d in range(DIMENSION):
+    # Initial position constraint
+    CONST_X_INIT[d] = model.addLConstr(x[0, d], '=', x_init[d], name=f"CONST_X_INIT[dim={d}]")
 
-# Constrains for ensuring goal is reached
-C1 = {}
-C2 = {}
+    # Initial velocity constraint
+    CONST_V_INIT[d] = model.addLConstr(x[1, d], '=', x[0, d] + v_init[d] + u[0, d], name=f"CONST_V_INIT[dim={d}]")
+
+# Constraints for detecting when a node reaches the goal.
+CONST_X_GOAL = {}
 
 for i in range(number_of_time_steps):
-    C1[i, 0] = model.addLConstr(x[i] - x_goal[0], '<=', R * (1 - b_goal[i]), name=f"C1_{i},0")
-    C1[i, 1] = model.addLConstr(x[i] - x_goal[0], '>=', -R * (1 - b_goal[i]), name=f"C1_{i},1")
-    C2[i, 0] = model.addLConstr(y[i] - x_goal[1], '<=', R * (1 - b_goal[i]), name=f"C2_{i},0")
-    C2[i, 1] = model.addLConstr(y[i] - x_goal[1], '>=', -R * (1 - b_goal[i]), name=f"C2_{i},1")
+    for d in range(DIMENSION):
+        CONST_X_GOAL[i, d, 0] = model.addLConstr(x[i, d] - x_goal[d], '<=', R * (1 - b_goal[i]), name=f"CONST_X_GOAL[time={i},dim={d},const=0]")
+        CONST_X_GOAL[i, d, 1] = model.addLConstr(x[i, d] - x_goal[d], '>=', -R * (1 - b_goal[i]), name=f"CONST_X_GOAL[time={i},dim={d},const=1]")
 
-model.addLConstr(gp.quicksum(b_goal[i] for i in range(number_of_time_steps)), '=', 1, name="C3")
+# Constraint for ensuring exactly one node reaches the goal.
+CONST_REACH_GOAL = model.addLConstr(gp.quicksum(b_goal[i] for i in range(number_of_time_steps)), '=', 1, name="CONST_REACH_GOAL")
 
-# # Constraints for ensuring the robot stays in the map
-#
-# C4 = {}
-# C5 = {}
-#
-# for i in range(number_of_time_steps):
-#     C4[i, 0] = model.addLConstr(x[i], '<=', x_lim[1], name=f"C4_{i},0")
-#     C4[i, 1] = model.addLConstr(x[i], '>=', x_lim[0], name=f"C4_{i},1")
-#     C5[i, 0] = model.addLConstr(y[i], '<=', y_lim[1], name=f"C5_{i},0")
-#     C5[i, 1] = model.addLConstr(y[i], '>=', y_lim[0], name=f"C5_{i},1")
+# Constraints for ensuring the robot does not collide with the obstacles.
+CONST_OBSTACLE = {}
+CONST_OBSTACLE_DETECTION = {}
 
-# Constraints for ensuring the robot does not collide with the obstacles
+for i in range(number_of_time_steps):
 
-C6 = {}
-C7 = {}
+    for k, obstacle in enumerate(list_of_obstacles):
 
-for i, obstacle in enumerate(list_of_obstacles):
-    for j in range(number_of_time_steps):
-        C6[i, j, 0] = model.addLConstr(x[j], '<=', obstacle.x_lower_left + R * b_in[i, 0, j], name=f"C6_{i},{j},0")
-        C6[i, j, 1] = model.addLConstr(x[j], '>=', obstacle.x_upper_right - R * b_in[i, 1, j], name=f"C6_{i},{j},1")
-        C7[i, j, 0] = model.addLConstr(y[j], '<=', obstacle.y_lower_left + R * b_in[i, 2, j], name=f"C7_{i},{j},0")
-        C6[i, j, 1] = model.addLConstr(y[j], '>=', obstacle.y_upper_right - R * b_in[i, 3, j], name=f"C7_{i},{j},1")
-        model.addLConstr(gp.quicksum(b_in[i, k, j] for k in range(4)), '<=', 3, name="C8")
+        for d in range(DIMENSION):
+
+            CONST_OBSTACLE[i, d, k, 0] = model.addLConstr(x[i, d], '<=', obstacle[0, d] + R * b_in[i, d, k, 0], name=f"CONST_OBSTACLE[time={i},dim={d},obs={k},bound={0}]")
+            CONST_OBSTACLE[i, d, k, 1] = model.addLConstr(x[i, d], '>=', obstacle[1, d] - R * b_in[i, d, k, 1], name=f"CONST_OBSTACLE[time={i},dim={d},obs={k},bound={1}]")
+
+        CONST_OBSTACLE_DETECTION[i, k] = model.addLConstr(gp.quicksum(b_in[i, d, k, 0] + b_in[i, d, k, 1] for d in range(DIMENSION)), '<=', 3, name=f"CONST_OBSTACLE_DETECTION[time={i},obs={k}]")
 
 # Constraints for ensuring the aircraft doesn't break laws of physics
-C8 = {}
-C9 = {}
-
-# for i in range(number_of_time_steps - 1):
-#     C8[i, 0] = model.addLConstr(x[i + 1] - x[i], '<=', v_max_x, name=f"C8_{i},0")
-#     C8[i, 1] = model.addLConstr(x[i + 1] - x[i], '>=', -v_max_x, name=f"C8_{i},1")
-#     C9[i, 0] = model.addLConstr(y[i + 1] - y[i], '<=', v_max_y, name=f"C9_{i},0")
-#     C9[i, 1] = model.addLConstr(y[i + 1] - y[i], '>=', -v_max_y, name=f"C9_{i},1")
+CONST_V_MAX = {}
+CONST_U_MAX = {}
 
 # Other constraints for ensuring the aircraft doesn't break laws of physics
 for i in range(number_of_time_steps - 1):
-    for j in range(normals):
+
+    for n in range(NORMALS):
         # Velocity constrain
-        C8[i, j] = model.addLConstr(
-            (x[i + 1] - x[i]) * math.cos(2 * math.pi / normals * j) + (y[i + 1] - y[i]) * math.sin(
-                2 * math.pi / normals * j), '<=', v_max, name=f"C8_{i},{j}")
+        CONST_V_MAX[i, n] = model.addLConstr(
+            (x[i + 1, 0] - x[i, 0]) * np.cos(2 * np.pi / NORMALS * n) + (x[i + 1, 1] - x[i, 1]) * np.sin(
+                2 * np.pi / NORMALS * n), '<=', v_max, name=f"COST_V_MAX[time={i},normal={n}]")
 
         # Input constrain
-        C9[i, j] = model.addLConstr(
-            (ux[i + 1] - ux[i]) * math.cos(2 * math.pi / normals * j) + (uy[i + 1] - uy[i]) * math.sin(
-                2 * math.pi / normals * j), '<=', u_max, name=f"C8_{i},{j}")
+        CONST_U_MAX[i, n] = model.addLConstr(
+            (u[i + 1, 0] - u[i, 0]) * np.cos(2 * np.pi / NORMALS * n) + (u[i + 1, 1] - u[i, 1]) * np.sin(
+                2 * np.pi / NORMALS * n), '<=', u_max, name=f"ONST_U_MAX[time={i},normals={n}]")
 
-C10 = {}
-C11 = {}
+# Relation input, velocity and position next point.
+CONST_POS = {}
 
 # Correlate input and the velocity
 for i in range(number_of_time_steps - 2):
-    C10[i] = model.addLConstr(x[i + 2], '=', 2 * x[i + 1] - x[i] + ux[i + 1], name=f"C10_{i}")
-    C11[i] = model.addLConstr(y[i + 2], '=', 2 * y[i + 1] - y[i] + uy[i + 1], name=f"C11_{i}")
+    for d in range(DIMENSION):
+        CONST_POS[i, d] = model.addLConstr(x[i + 2, d], '=', 2 * x[i + 1, d] - x[i, d] + u[i + 1, d], name=f"CONST_POS[time={i},dim={d}]")
 
-model.setObjective(gp.quicksum(b_goal[i] * i for i in range(number_of_time_steps)), gp.GRB.MINIMIZE)
+OBJECTIVE = model.setObjective(gp.quicksum(b_goal[i] * i for i in range(number_of_time_steps)), gp.GRB.MINIMIZE)
 
 model.update()
 
@@ -164,35 +138,40 @@ model.write("models/model.lp")
 
 model.optimize()
 
-x_result = []
-y_result = []
+# ============== PLOT RESULTS =================================
 
-for i in range(number_of_time_steps):
-    x_result.append(x[i].x)
-    y_result.append(y[i].x)
+# Define figure and axis.
+fig, ax = plt.subplots()
 
+# Set limits
+ax.set_xlim(map_bound[0, 0], map_bound[1, 0])
+ax.set_ylim(map_bound[0, 1], map_bound[1, 1])
 
-# plt.plot(x_result, y_result, marker='.')
-
+# Construct list with vehicle location to be plotted
 x_plot = []
 y_plot = []
 
-for i in range(len(y_result)):
-    if math.isclose(y_result[i], x_goal[1], abs_tol=1e-9) and math.isclose(x_result[i], x_goal[0], abs_tol=1e-9):
+for i in range(number_of_time_steps):
+    x_plot.append(x[i, 0].X)
+    y_plot.append(x[i, 1].X)
+
+    # Stop plot once goal is reached.
+    if b_goal[i].X == 1:
         break
 
-    x_plot.append(x_result[i])
-    y_plot.append(y_result[i])
+# Plot vehicle location
+ax.plot(x_plot, y_plot, marker=".", color='red', label="Path")
+ax.quiver(x_plot, y_plot, marker=".", color='k', label="Path")
 
+
+# Plot obstacles
 for obstacle in list_of_obstacles:
-    x = [obstacle.x_upper_right, obstacle.x_upper_right,
-         obstacle.x_lower_left, obstacle.x_lower_left, obstacle.x_upper_right]
-    y = [obstacle.y_upper_right, obstacle.y_lower_left,
-         obstacle.y_lower_left, obstacle.y_upper_right, obstacle.y_upper_right]
-    plt.plot(x, y)
-plt.xlim(x_lim)
-plt.ylim(y_lim)
+    origin = obstacle[0]
+    delta = obstacle[1] - obstacle[0]
+    width = delta[0]
+    height = delta[1]
 
-plt.plot(x_plot, y_plot, marker='.', label="Path")
+    ax.add_patch(Rectangle(origin, width, height, color='dimgrey'))
 
+#display plot
 plt.show()
